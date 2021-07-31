6190e0dea3a164b30b8e27d2096738e59180abbb
Chakrig14 <chakrig14@users.noreply.github.com>
2021-08-01 01:24:03 +0530

    login check

--- src/components/capital/index.js
+++
@@ -1,9 +0,0 @@
-const Capital = props => {
-    const {capitalDeatils} = props
-    const {capitalName} = capitalDeatils 
-    return(
-        <p>{capitalName}</p>
-    )
-}
-
-export default Capital
